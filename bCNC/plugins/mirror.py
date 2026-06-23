197f18a07a8154f6bfa8ffb670097547e2784438
Vladislav Rishe <vladr08@gmail.com>
2026-06-23 12:30:57 +0300

    Add mirror CAM plugin

---
+++ bCNC/plugins/mirror.py
@@ -0,0 +1,132 @@
+import math
+from ToolsPage import Plugin
+from CNC import Block, CNC
+
+try:
+    _
+except NameError:
+    def _(s): return s
+
+class Tool(Plugin):
+    __doc__ = _("Mirror selected G-code blocks around their collective selection center or document center")
+
+    def __init__(self, master):
+        Plugin.__init__(self, master, "Mirror")
+        self.icon = "mirror"
+        self.group = "CAM"
+        self.variables = [
+            ("axis", "X,Y", "X", _("Axis"), _("Axis to flip across (X or Y)")),
+            ("pivot_mode", "Document, Selected, Offset", "Document", _("Pivot Mode"), _("Check for doc center, uncheck for collective selection center")),
+            ("pivot_ofs", "mm", 0.0, _("Pivot Offset"), _("Mirror pivot offset along selected axis")),
+        ]
+        self.help = """This plugin mirrors the selected code blocks.
+
+Field descriptions:
+===================
+- Axis: An axis to mirror around. 
+- Pivot Mode:
+   - Document: the entire document bounds center is the mirror pivot
+   - Selected: selected blocks bounds center is used for mirroring
+   - Offset: the mirror pivot is offset by 'Pivot Offset' along the selected 'Axis'
+- Pivot Offset: The distance the mirror pivot is offset along selected 'Axis'
+"""
+        self.buttons.append("exe")
+
+    @staticmethod
+    def _get_margins(gcode, items):
+        # Get bounding box of document
+        minx, miny, maxx, maxy = 0, 0, 0, 0 # BUG!
+        for bid, lid in items:
+            paths = gcode.toPath(bid)
+            for path in paths:
+                minx2, miny2, maxx2, maxy2 = path.bbox()
+                minx, miny, maxx, maxy = (
+                    min(minx, minx2),
+                    min(miny, miny2),
+                    max(maxx, maxx2),
+                    max(maxy, maxy2),
+                )
+        return minx, miny, maxx, maxy
+
+    @staticmethod
+    def _get_margins2(gcode, items=None):
+        # Get bounding box of document
+        minx, miny, maxx, maxy = float('inf'), float('inf'), float('-inf'), float('-inf')
+        blocks = gcode.blocks
+        if items is not None:
+            blocks = [gcode.blocks[bid] for bid in set([bid for bid, _ in items])]
+        for block in blocks:
+            for line in block:
+                cmds = CNC.parseLine(line.upper())
+                if cmds is None:
+                    continue
+                for cmd in cmds:
+                    try:
+                        if cmd[0] in "XI":
+                            val = float(cmd[1:]) * gcode.cnc.unit
+                            minx = min(minx, val)
+                            maxx = max(maxx, val)
+                        elif cmd[0] in "YJ":
+                            val = float(cmd[1:]) * gcode.cnc.unit
+                            miny = min(miny, val)
+                            maxy = max(maxy, val)
+                    except Exception:
+                        continue
+        return (
+            minx if not math.isinf(minx) else 0,
+            miny if not math.isinf(miny) else 0,
+            maxx if not math.isinf(maxx) else 0,
+            maxy if not math.isinf(maxy) else 0
+        )
+
+    def execute(self, app):
+        app.setStatus(_("Mirror: Extracting coordinates..."))
+
+        axis = self["axis"]
+        mode = self['pivot_mode']
+
+        blocks_sel = app.editor.getCleanSelection()
+        if not blocks_sel:
+            app.setStatus(_("Mirror: ERROR! No blocks selected."))
+            return
+
+        if mode == 'Offset':
+            cx = cy = 2 * self.fromMm("pivot_ofs")
+        else:
+            if mode == 'Document':
+                if not CNC.isMarginValid():
+                    app.setStatus("Mirror: ERROR! GCode margin invalid.")
+                xmin, ymin, xmax, ymax = (
+                    CNC.vars["xmin"], CNC.vars["ymin"],
+                    CNC.vars["xmax"], CNC.vars["ymax"]
+                )
+            else:
+                xmin, ymin, xmax, ymax = self._get_margins2(app.gcode, blocks_sel)
+            cx = (xmin + xmax)# / 2.0
+            cy = (ymin + ymax)# / 2.0
+
+        if axis == "X":
+            mod_args = {'X':cx, 'I':0}
+        elif axis == "Y":
+            mod_args = {'Y':cy, 'J':0}
+        else:
+            app.setStatus(_("Mirror: ERROR! Unsupported axis."))
+            return
+
+        processed_count = 0
+        def mod_func(new, old, relative, *args):
+            ofs = args[0]
+            changed = False
+            for axis, C in ofs.items():
+                if axis in new:
+                    new[axis] = C - new[axis]
+                    nonlocal processed_count
+                    processed_count += 1
+                    changed = True
+            if changed and app.gcode.cnc.gcode in (2, 3):  # Change  2<->3
+                app.gcode.cnc.gcode = 5 - app.gcode.cnc.gcode
+            return changed
+        app.gcode.modify(blocks_sel, mod_func, None, mod_args)
+        if processed_count > 0:
+            app.refresh()
+        app.setStatus(_("Mirror: done ({} blocks mirrored around {},{})").format(processed_count, cx, cy))
